6e4aa5582e67e0326fe66a03ae4ee67b43165def
James Kariuki <karisjaymo99@gmail.com>
2023-03-28 18:28:20 +0000

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""This module defines a square"""
+
+
+class Square:
+    """Represents a mathematical square
+
+    Args:
+        size (int): The size of the square
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''
+        Calculates the area of a square
+        '''
+        return (self.__size ** 2)
