e8336daced8308bbcb3816a5fd53692019a8fce5
James Kariuki <karisjaymo99@gmail.com>
2023-04-15 14:58:49 +0000

    Task 6 - unittest for max_integer()

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Module to implement unittests
+'''
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    '''A Test case
+    '''
+
+    def test_values(self):
+        self.assertIsNone(max_integer())
+        self.assertEqual(max_integer([10, 50, 30, 40]), 50)
+        self.assertEqual(max_integer([10, -50, 30, 40]), 40)
+        self.assertEqual(max_integer([-10, -50, -30, -40]), -10)
+        self.assertEqual(max_integer("snake"), "s")
+        self.assertRaises(TypeError, max_integer, 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
