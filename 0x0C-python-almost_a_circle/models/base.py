e3c3938b761461aff6cc8ee65438a2c648a60ac3
James Kariuki <karisjaymo99@gmail.com>
2023-04-30 07:28:01 +0000

    add @staticmethod decorator to Base.to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,6 +15,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         '''serializes an Python object to JSON string representation.
 
