ce37105862fce5f49dd29f6aec75fb162dffe4f3
James Kariuki <karisjaymo99@gmail.com>
2023-05-17 11:18:41 +0300

    count num of records with same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- lists number of records with the same `score` from `second_table`
-SELECT `score`, COUNT(`score`) AS "score"
+SELECT `score`, COUNT(`score`) AS "number"
 FROM `second_table`
-GROUP BY `score`;
+GROUP BY `score`
+ORDER BY `number` DESC;
