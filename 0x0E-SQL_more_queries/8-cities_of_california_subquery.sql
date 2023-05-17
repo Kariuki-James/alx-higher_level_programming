2254bba9a140e625669a09d47c8ab535ddeb7486
James Kariuki <karisjaymo99@gmail.com>
2023-05-17 19:47:17 +0300

    add Task 3 to 8, more queries

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- list all the cities of California
+SELECT `id`, `name`
+FROM `cities`
+WHERE cities.state_id = (
+	SELECT `id`
+	FROM `states`
+	WHERE `name` = 'California')
+ORDER BY cities.id ASC;
