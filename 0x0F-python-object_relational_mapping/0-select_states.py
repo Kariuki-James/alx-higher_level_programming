0e13807208e0d5a59554a71ad40495a5b3945dca
James Kariuki <karisjaymo99@gmail.com>
2023-06-17 13:48:45 +0300

    structure 0-select_states module using functions

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
-'''MySQLdb connection to insert states
+'''0-select_states module
+Lists all states from hbtn_0e_0_usa.states
 '''
 
+from sys import argv
 import MySQLdb
-import sys
 
-if __name__ != "__main__":
-    sys.exit()
 
-num_args = len(sys.argv)
-if num_args < 4:
-    print("3 arguments required: <username> <passwd> <dbname>")
-    sys.exit()
+def list_all_states(username, password, dbname):
+    '''lists all states from hbtn_0e_0_usa.states
 
-conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
-        )
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-result = cur.fetchall()
-for row in result:
-    print(row)
-cur.close()
-conn.close()
+    Args:
+        username (str): mysql username
+        password (str): mysql password
+        dbname (str): mysql database name
+    '''
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=dbname,
+            charset="utf8"
+            )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    num_args = len(argv)
+    if num_args < 4:
+        print("3 arguments required: <username> <password> <dbname>")
+    else:
+        list_all_states(argv[1], argv[2], argv[3])
