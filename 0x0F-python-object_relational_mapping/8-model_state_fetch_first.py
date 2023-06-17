1f60d03d0900ba1aef9b36ad53eb374cdea9394e
James Kariuki <karisjaymo99@gmail.com>
2023-06-17 20:32:42 +0300

    fetch first state object

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,20 +8,18 @@ from sqlalchemy.orm import sessionmaker
 
 from model_state import Base, State
 
-if __name__ != "__main__":
-    quit()
-
 if len(sys.argv) < 4:
     print("3 args required: <db-username> <db-passwd> <db-name>")
     sys.exit()
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-query = session.query(State).order_by(State.id.asc())
-result = query.first()
-print(f'{result.id}:', result.name)
+    query = session.query(State).order_by(State.id.asc())
+    result = query.first()
+    print(f'{result.id}:', result.name)
