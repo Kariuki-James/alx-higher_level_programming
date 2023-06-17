cb13aff50194faaca5fa3c32b1630ddbe98440e5
James Kariuki <karisjaymo99@gmail.com>
2023-06-17 20:29:01 +0300

    create function 'list_states'

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,20 +9,24 @@ from sqlalchemy.orm import sessionmaker
 
 from model_state import Base, State
 
-if __name__ != "__main__":
-    sys.exit()
 
-if len(sys.argv) < 4:
-    print("3 args required: <db-username> <db-passwd> <db-name>")
-    sys.exit()
+def list_states():
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    query = session.query(State).order_by(State.id.asc())
+    for state in query.all():
+        print(f'{state.id}:', state.name)
 
-query = session.query(State).order_by(State.id.asc())
-for state in query.all():
-    print(f'{state.id}:', state.name)
+    session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("3 args required: <db-username> <db-passwd> <db-name>")
+        sys.exit()
+    list_states()
