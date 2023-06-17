8befa39da686e3ba227f38d094e9c1f8538813d5
James Kariuki <karisjaymo99@gmail.com>
2023-06-17 20:06:05 +0300

    prevent execution if module is imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,20 +2,21 @@
 '''Lists all states in a table
 '''
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
 
-from model_state import Base, State
+    from model_state import Base, State
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                        sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-query = session.query(State).order_by(State.id.asc())
-for state in query.all():
-    print(f'{state.id}:', state.name)
+    query = session.query(State).order_by(State.id.asc())
+    for state in query.all():
+        print(f'{state.id}:', state.name)
