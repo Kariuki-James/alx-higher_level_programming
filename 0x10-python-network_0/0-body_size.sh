520870681dae437cd721ccae6f8c621ec3480596
James Kariuki <karisjaymo99@gmail.com>
2023-07-02 09:10:57 +0300

    reduce '0-body_size.sh' to 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # sends a request to a URL, and displays the size of the response body.
-
-if [[ $# -lt 1 ]]; then
-    echo "Error: No URL provided."
-    echo "Usage: ./0-body_size.sh <URL>"
-    exit 1
-fi
-
-response_header=$(curl -sI "$1")
-content_length=$(echo "$response_header" | grep -m 1 -i "content-length" | awk '{ print $2 }')
-echo "$content_length"
+curl -sI "$1" | grep -m 1 -i "content-length" | awk '{ print $2 }'
