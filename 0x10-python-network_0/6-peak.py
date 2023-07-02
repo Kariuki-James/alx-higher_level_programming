bb839d45a8549c8d9ce32a380cdf0293eba6f800
James Kariuki <karisjaymo99@gmail.com>
2023-07-02 19:26:14 +0300

    update python script documentation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+'''Module defines function to fine peak in a list
+'''
 
 def find_peak(list_of_integers):
+    '''Finds a peak in a list of unsorted integers
+
+    Args:
+        list_of_integers (list): list of unsorted integers.
+    '''
     list_start = 0
     list_len = len(list_of_integers)
     list_end = list_len - 1
