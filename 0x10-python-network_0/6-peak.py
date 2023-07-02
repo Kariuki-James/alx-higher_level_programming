a318cfd5071f57707d05d3be427f7a26bc4aa656
James Kariuki <karisjaymo99@gmail.com>
2023-07-02 19:10:52 +0300

    add '6-peak.py'

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+
+def find_peak(list_of_integers):
+    list_start = 0
+    list_len = len(list_of_integers)
+    list_end = list_len - 1
+
+    if list_len < 1:
+        return None
+
+    while list_start < list_end:
+        mid = (list_start + list_end) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            list_start = mid + 1
+        else:
+            list_end = mid
+
+    return list_of_integers[list_start]
